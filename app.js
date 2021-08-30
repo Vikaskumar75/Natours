fbe9da31f140dd38d709465a857b38d531e10259
Vikas <vikas@bigheadsoftwares.com>
2021-08-31 00:56:45 +0530

    learnt get push and find by id apis

---
+++ app.js
@@ -0,0 +1,65 @@
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side!!', app: 'natours' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.status(200).send('You can post to this url');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: tour,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      if (err) console.log(err);
+      res.status(201).json({ status: 'success', data: { tour: newTour } });
+    }
+  );
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`listening on port ${port}......`);
+});
